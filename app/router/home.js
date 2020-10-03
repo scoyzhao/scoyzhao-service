1a90160ac4156e81867319715b1b5c7046576aa6
zhaoyi <scoyzhao@163.com>
2020-10-04 00:34:32 +0800

    feat: project init

---
+++ app/router/home.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: scoyzhao
+ * @Date: 2020-10-04 00:29:15
+ * @Last Modified by: scoyzhao
+ * @Last Modified time: 2020-10-04 00:32:00
+ */
+
+'use strict';
+
+module.exports = app => {
+  const { router, controller } = app;
+  router.get('/home', controller.home.index.index);
+};
