e661fbbb11dcf4d4522f94cf832b0a46f7ec01b0
zhaoyi <scoyzhao@163.com>
2020-10-16 01:39:07 +0800

    feat: add test logout api

--- app/router/home.js
+++ app/router/home.js
@@ -2,12 +2,15 @@
  * @Author: scoyzhao
  * @Date: 2020-10-04 00:29:15
  * @Last Modified by: scoyzhao
- * @Last Modified time: 2020-10-13 21:48:03
+ * @Last Modified time: 2020-10-16 01:38:35
  */
 
 'use strict';
 
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/home', controller.home.index.index);
+  const auth = app.middleware.auth();
+
+  // * 测试auth接口
+  router.get('/home', auth, controller.home.index.index);
 };
